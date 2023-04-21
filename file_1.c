e1d724f62e8807954111458e16d220ac80f513bd
DubsG <gichuhigeorge041@gmail.com>
2023-04-21 08:08:16 -0700

    function printing the name of user

---
+++ file_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print - prints name of the user followed by a new line
+ * Return: 0 on success
+ */
+int print(void)
+{
+	printf("DubsEmpire,\n");
+
+	return (0);
+}
